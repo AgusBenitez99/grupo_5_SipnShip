903df276d1e4ec69c7d7776f0fbca99b2a96d53a
AgusBenitez99 <agustin.newells99@gmail.com>
2023-07-16 16:05:45 -0300

    armado de header

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ app.use(express.static('public'));
 app.get("/", (req, res) =>
   res.sendFile(path.join(__dirname, "views", "home.html"))
 );
+app.get("/header", (req, res) =>
+  res.sendFile(path.join(__dirname, "views", "partials/header.html"))
+);
 
 app.listen(PORT, () =>
   console.log("Server running in http://localhost:" + PORT)
