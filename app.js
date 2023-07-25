74c1e31b9331ce1f40e989837fc50c6a31962fd1
AgusBenitez99 <agustin.newells99@gmail.com>
2023-07-24 23:52:06 -0300

    feat: html & css register

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ app.get("/", (req, res) =>
 app.get("/header", (req, res) =>
   res.sendFile(path.join(__dirname, "views", "partials/header.html"))
 );
+app.get("/register", (req, res) =>
+  res.sendFile(path.join(__dirname, "views", "register.html"))
+);
+
 
 app.listen(PORT, () =>
   console.log("Server running in http://localhost:" + PORT)
