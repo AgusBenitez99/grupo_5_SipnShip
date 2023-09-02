c42db914346f7e475da2a65fc0ad118725bd2c2e
GonzaloArmoa <gonzalo-armoa@hotmail.com>
2023-09-02 10:14:57 -0300

    feat: validaciones en loginValidation.js listas !

--- src/validations/loginValidator.js
+++ src/validations/loginValidator.js
@@ -1,9 +1,21 @@
-const {check} = require ("express-validator")
+const {check,body} = require ("express-validator")
 const {readJSON} = require ('../../data');
 const {compareSync} = require('bcryptjs')
 
 module.exports = [
-    body()
+    check('email')
+        .notEmpty().withMessage('Coloca tu email papu!').bail()
+        .isEmail().withMessage('Capo.. tiene que ser un email!').bail(),
+    body('password')
+        .notEmpty().withMessage('Aca va la password amigo!').bail()
+        .custom((value, {req}) => {
+            const users = readJSON('user.json');
+            const user = users.find(user => user.email === req.body.email);
 
+            if( !user || !compareSync(value, user.passwood)) {
+                return false
+            }
+                return true
+        }).withMessage ('Acá hay algo raaaroo ... revisa los datos o registrate !')
 
 ]
