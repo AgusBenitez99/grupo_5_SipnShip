76cf08433324b9d3040499b441a3530e42cd7c0d
GonzaloArmoa <gonzalo-armoa@hotmail.com>
2023-09-02 09:27:38 -0300

    feat: agrego archivo de validacion. listo!

---
+++ src/validations/loginValidator.js
@@ -0,0 +1,7 @@
+const {check} = require ("express-validator");
+const {readJSON} = require ('../data');
+
+module.exports = [
+
+
+]
---
+++ src/validations/registerValidator.js
@@ -0,0 +1,6 @@
+const {check} = require('express-validator');
+const {readJSON} = require('../data');
+
+module.exports = [
+    
+]
