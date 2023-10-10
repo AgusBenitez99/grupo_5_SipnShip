2b1e6a0adf2cd2281330000947fbbb766d311c25
AgusBenitez99 <agustin.newells99@gmail.com>
2023-10-10 18:24:31 -0300

    agregado de script para password

---
+++ public/javascripts/passwordEye.js
@@ -0,0 +1,14 @@
+const pass = document.getElementById("password"),
+icon = document.querySelector(".fa-eye")
+
+icon.addEventListener("click", e => {
+    if (pass.type === "password") {
+        pass.type = "text";
+        icon.classList.remove('fa-eye')
+        icon.classList.add('fa-eye-slash')
+    } else {
+        pass.type = "password"
+        icon.classList.add('fa-eye')
+        icon.classList.remove('fa-eye-slash')
+    }
+})
