e809c22e39d09c99eaf1d8f7944df0091c1715d0
maurobustos5081 <mauro_18_b@hotmail.com>
2023-10-24 00:27:33 -0300

    create and delete products

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -6,123 +6,169 @@ const { validationResult } = require('express-validator');
 
 module.exports = {
   edit: (req, res) => {
-  
-    const product=db.Product.findByPk(req.params.id,{
-      include : ['brand','section','category']
+
+    const product = db.Product.findByPk(req.params.id, {
+      include: ['brand', 'section', 'category']
     })
-    const section=db.Section.findAll({order : ['name']})
-    const category=db.Category.findAll({order : ['name']})
-    Promise.all([product, category,section])
-    .then(([product, category,section])=>{
-      return res.render('product/edit', {
-      ...product.dataValues,
-      category,
-      section
-    })
-    }).catch((error) => console.log(error));
-    
+    const section = db.Section.findAll({ order: ['name'] })
+    const category = db.Category.findAll({ order: ['name'] })
+    Promise.all([product, category, section])
+      .then(([product, category, section]) => {
+        return res.render('product/edit', {
+          ...product.dataValues,
+          category,
+          section
+        })
+      }).catch((error) => console.log(error));
+
   },
   update: (req, res) => {
-   
-  const { name, brand,category, section, description, price, discount,size } = req.body;
 
-  db.Product.findByPk(req.params.id, {
-    include: ["images"],
-  })
-    .then((product) => {
-      req.files.image &&
-        existsSync(`./public/images/${product.image}`) &&
-        unlinkSync(`./public/images/${product.image}`);
+    const { name, brand, category, section, description, price, discount, size } = req.body;
 
-      db.Product.update(
-        {
-          name: name.trim(),
-          price,
-          size,
-          discount,
-          //brandId: brand,
-          sectionId: section,
-          categoryId:category,
-          //stock
-          description: description.trim(),
-          mainImage: req.files.image ? req.files.image[0].filename : product.image,
-        },
-        {
-          where: {
-            id:req.params.id,
-          },
-        }
-      ).then(() => {
-        if (req.files.images) {
-          product.images.forEach((image) => {
-            existsSync(`./public/images/${image.file}`) &&
-              unlinkSync(`./public/images/${image.file}`);
-          });
-
-          db.Image.destroy({
-            where: {
-              productId: req.params.id,
-            },
-          }).then(() => {
-            const images = req.files.images.map((file) => {
-              return {
-                file: file.filename,
-                main: false,
-                productId: product.id,
-              };
-            });
-            db.Image.bulkCreate(images, {
-              validate: true,
-            }).then((response) => {
-              return res.redirect("/admin");
-            });
-          });
-        } else {
-          return res.redirect("/admin");
-        }
-      });
+    db.Product.findByPk(req.params.id, {
+      include: ["images"],
     })
-    .catch((error) => console.log(error));
+      .then((product) => {
+        req.files.image &&
+          existsSync(`./public/images/${product.image}`) &&
+          unlinkSync(`./public/images/${product.image}`);
+
+        db.Product.update(
+          {
+            name: name.trim(),
+            price,
+            size,
+            discount,
+            //brandId: brand,
+            sectionId: section,
+            categoryId: category,
+            //stock
+            description: description.trim(),
+            mainImage: req.files.image ? req.files.image[0].filename : product.image,
+          },
+          {
+            where: {
+              id: req.params.id,
+            },
+          }
+        ).then(() => {
+          if (req.files.images) {
+            product.images.forEach((image) => {
+              existsSync(`./public/images/${image.file}`) &&
+                unlinkSync(`./public/images/${image.file}`);
+            });
+
+            db.Image.destroy({
+              where: {
+                productId: req.params.id,
+              },
+            }).then(() => {
+              const images = req.files.images.map((file) => {
+                return {
+                  file: file.filename,
+                  main: false,
+                  productId: product.id,
+                };
+              });
+              db.Image.bulkCreate(images, {
+                validate: true,
+              }).then((response) => {
+                return res.redirect("/admin");
+              });
+            });
+          } else {
+            return res.redirect("/admin");
+          }
+        });
+      })
+      .catch((error) => console.log(error));
   },
   new: (req, res) => {
     return res.render('product/new')
   },
   create: (req, res) => {
-    const products = readJSON("products.json");
 
-    const data = {
-      ...req.body,
-      mainImage: req.files.mainImage ? req.files.mainImage[0].filename : null,
-      images: req.files.images
-        ? req.files.images.map((image) => image.filename)
-        : [],
+    const errors = validationResult(req);
+
+    if (errors.isEmpty()) {
+
+      const { name, price, size, description, category, brand, mainImage } = req.body
+
+      db.Product.create({
+        name: name.trim(),
+        price,
+        size,
+        description: description.trim(),
+        categoryId: category,
+        //brandId : brand,
+        mainImage: req.files.mainImage ? req.files.mainImage[0].filename : null
+      })
+        .then(product => {
+
+
+          if (req.files.images) {
+            const images = req.files.images.map((file) => {
+              return {
+                file: file.filename,
+                productId: product.id,
+              }
+            })
+
+            db.Image.bulkCreate(images, {
+              validate: true
+            }).then(response => {
+              return res.redirect('/admin');
+            })
+          } else {
+            return res.redirect('/admin');
+
+          }
+        })
+        .catch(error => console.log(error))
+
+    } else {
+
+      if (req.files.length) {
+        req.files.forEach(file => {
+          existsSync('./public/images/' + file.filename) && unlinkSync('./public/images/' + file.filename)
+        });
+      }
+
+      const sections = db.Section.findAll({
+        order: ['name']
+      });
+
+      Promise.all([sections])
+        .then(([sections]) => {
+          return res.render("product/new", {
+            sections,
+            errors: errors.mapped(),
+            old: req.body
+          });
+        })
+        .catch(error => console.log(error))
+
     }
-
-    let newProduct = new Product(data);
-    products.push(newProduct);
-
-    writeJSON(products, 'products.json');
-
-    return res.redirect('/admin');
   },
   detail: (req, res) => {
 
     const id = req.params.id
-        
-   const product = db.Product.findByPk(req.params.id,{
-    include : ['brand','section','category','images']
-  })
 
-   const products = db.Product.findAll({
-    include : ['brand','section','category']
-})
+    const product = db.Product.findByPk(req.params.id, {
+      include: ['brand', 'section', 'category', 'images']
+    })
+
+    const products = db.Product.findAll({
+      include: ['brand', 'section', 'category']
+    })
 
     Promise.all([product, products])
       .then(([product, products]) => {
         return res.render("product/detail", {
           products,
           ...product.dataValues,
-      });
+        });
       })
       .catch(error => console.log(error))
   },
@@ -131,28 +177,21 @@ module.exports = {
     return res.render('product/trolley')
   },
   remove: (req, res) => {
+    db.Image.destroy({
+      where: {
+        productId: req.params.id
+      }
+    })
+      .then(() => {
+        db.Product.destroy({
+          where: {
+            id: req.params.id
+          }
+        }).then(() => {
+          return res.redirect('/admin')
+        })
+      })
+      .catch(error => console.log(error));
 
-    const products = readJSON('products.json');
-    const id = req.params.id;
-    const product = products.find(product => product.id === id)
-    const productModify = products.filter(product => product.id !== id);
-    
-    if (product.images) {
-      product.images.forEach(image => {
-        const imagePath = `./public/images/${image}`;
-
-        if (existsSync(imagePath)) {
-          unlinkSync(imagePath);
-        }
-      });
-    }
-
-    existsSync(`./public/images/${product.mainImage}`) && unlinkSync(`./public/images/${product.mainImage}`)
-
-
-
-
-    writeJSON(productModify, 'products.json')
-    return res.redirect('/admin')
   }
 }
---
+++ src/validations/productAddValidator.js
@@ -0,0 +1,24 @@
+const {check, body} = require('express-validator');
+
+module.exports = [
+    check('name')
+        .notEmpty().withMessage('El nombre del producto es obligatorio'),
+    check('section')
+        .notEmpty().withMessage('La sección es requerida'),
+    check('price')
+        .notEmpty().withMessage('Debes indicar el precio').bail()
+        .isDecimal().withMessage('El precio debe ser un número'),
+    check('description')
+        .notEmpty().withMessage('La descripción es requerida').bail()
+        .isLength({
+            min : 20,
+            max : 800
+        }).withMessage('La descripción debe tener entre 20 y 500 caracteres'),
+    body('images')
+        .custom((value, {req}) => {
+           if(req.files.image){
+                return true
+           }
+           return false
+        }).withMessage('No has subido ninguna imagen')
+]
