4698ec6bcb03529338930306e4c680eff6ec2e0a
Rodrigo Alvarez <rodrigodemianalvarez@gmail.com>
2023-11-21 09:36:38 -0300

    modificacion edit ejs y js editvalidation

--- public/javascripts/editValidation.js
+++ public/javascripts/editValidation.js
@@ -1,124 +1,114 @@
-const $ = id => document.getElementById(id);
+        const $ = id => document.getElementById(id);
 
 window.onload = function () {
-
     $('name').addEventListener('focus', function (e) {
-        $('msgError-name').innerHTML = null
-        this.classList.remove('is-invalid')
-        this.classList.remove('is-valid')
-    })
+        $('msgError-name').innerHTML = null;
+    });
+
     $('name').addEventListener('blur', function (e) {
+        const nameValue = this.value.trim();
 
         switch (true) {
-            case !this.value.trim():
-                $('msgError-name').innerHTML = "El nombre es obligatorio"
-                this.classList.add('is-invalid')
+            case !nameValue:
+                $('msgError-name').innerHTML = "El nombre es obligatorio";
                 break;
-            case this.value.trim().length < 2:
+            case nameValue.length < 2:
                 $('msgError-name').innerHTML = "Mínimo dos letras";
-                this.classList.add('is-invalid')
-                break
-            case !/^[ a-zA-ZñÑáéíóúÁÉÍÓÚ]+$/.test(this.value.trim()):
+                break;
+            case !/^[ a-zA-ZñÑáéíóúÁÉÍÓÚ]+$/.test(nameValue):
                 $('msgError-name').innerHTML = "Solo se permiten letras";
-                this.classList.add('is-invalid')
-                break
+                break;
+            case /\d/.test(nameValue):
+                $('msgError-name').innerHTML = "No debe contener caracteres numéricos";
+                break;
             default:
                 $('msgError-name').innerHTML = null;
-                this.classList.add('is-valid')
-                this.classList.remove('is-invalid')
-                break;
-        }
-    });
-    /*  $('categoria').addEventListener('blur', function (e) {
-         switch (true) {
-             case !this.value.trim():
-                 $('msgError-categoria').innerHTML = "Debes seleccionar una categoría";
-                 this.classList.add('is-invalid');
-                 break;
-             default:
-                 $('msgError-categoria').innerHTML = null;
-                 this.classList.add('is-valid');
-                 this.classList.remove('is-invalid');
-                 break;
-         }
-     }); */
-    $('price').addEventListener('blur', function (e) {
-        switch (true) {
-            case !this.value.trim():
-                $('msgError-price').innerHTML = "El precio es obligatorio";
-                this.classList.add('is-invalid');
-                break;
-            case isNaN(this.value.trim()) || parseFloat(this.value.trim()) <= 0:
-                $('msgError-price').innerHTML = "El precio debe ser mayor que 0";
-                this.classList.add('is-invalid');
-                break;
-            default:
-                $('msgError-price').innerHTML = null;
                 this.classList.add('is-valid');
                 this.classList.remove('is-invalid');
                 break;
         }
-    })
-    $('size').addEventListener('blur', function (e) {
-        switch (true) {
-            case !this.value.trim():
-                $('msgError-size').innerHTML = "El tamaño es obligatorio";
-                this.classList.add('is-invalid');
-                break;
-            default:
-                $('msgError-size').innerHTML = null;
-                this.classList.add('is-valid');
-                this.classList.remove('is-invalid');
-                break;
-        }
-    });
-    $('editForm').addEventListener('submit', function (event) {
-        event.preventDefault();
-
-        const elementsForm = this.elements; 
-        let error = false; 
-
-        for (let i = 0; i < elementsForm.length - 1; i++) { 
-
-            if (!elementsForm[i].value.trim()) { 
-
-                error = true; 
-            $('msgError-empty').innerHTML = "El formulario tiene errors" 
-        }
+        $('price').addEventListener('blur', function (e) {
+            const priceValue = this.value.trim();
     
-    } 
-    !error && this.submit()
-    })
+            switch (true) {
+                case !priceValue:
+                    $('msgError-price').innerHTML = "El precio es obligatorio";
+                    break;
+                case isNaN(priceValue) || parseFloat(priceValue) <= 0:
+                    $('msgError-price').innerHTML = "El precio debe ser mayor que 0";
+                    break;
+                default:
+                    $('msgError-price').innerHTML = null;
+                    this.classList.add('is-valid');
+                    this.classList.remove('is-invalid');
+                    break;
+            }
+        });
+        $('size').addEventListener('blur', function (e) {
+            const sizeValue = this.value.trim();
+    
+            switch (true) {
+                case !sizeValue:
+                    $('msgError-size').innerHTML = "El tamaño es obligatorio";
+                    break;
+                case isNaN(sizeValue) || parseFloat(sizeValue) <= 0:
+                    $('msgError-size').innerHTML = "El tamaño debe ser mayor que 0";
+                    break;
+                default:
+                    $('msgError-size').innerHTML = null;
+                    break;
+            }
+        });
+    });
+
+    document.getElementById('editForm').addEventListener('submit', function (e) {
+       
+        var hayErrores = false;
+//validacion de nombre
+        var nameValue = $('name').value.trim();
+        if (!nameValue || nameValue.length < 2 || !/^[ a-zA-ZñÑáéíóúÁÉÍÓÚ]+$/.test(nameValue) || /\d/.test(nameValue)) {
+            $('msgError-name').innerHTML = "Error en el nombre";
+            $('name').classList.add('is-invalid');
+            hayErrores = true;
+        } else {
+            $('msgError-name').innerHTML = null;
+            $('name').classList.add('is-valid');
+            $('name').classList.remove('is-invalid');
+        }
+
+        //validacion de precio
+        var priceValue = $('price').value.trim();
+        if (priceValue==="" || isNaN(priceValue) || parseFloat(priceValue) <= 0) {
+            $('msgError-price').innerHTML = "Error en el precio";
+            hayErrores = true;
+        } else {
+            $('msgError-price').innerHTML = null;
+        }
+
+        //validacion de tamaño
+        var sizeValue = $('size').value.trim();
+        if (sizeValue === "" || isNaN(sizeValue)) {
+            $('msgError-size').innerHTML = "El tamaño es obligatorio y debe ser un número";
+            hayErrores = true;
+        } else {
+            $('msgError-size').innerHTML = null;
+        }
+
+        //validacion de descripcion
+        var descriptionValue = $('descripcion').value.trim();
+        console.log("Description Value: ", descriptionValue);
+    if (descriptionValue === "") {
+        console.log("Error: La descripción está vacía");
+        $('msgError-description').innerHTML = "La descripción es obligatoria";
+        hayErrores = true;
+    } else {
+        console.log("Error: La descripción es valida");
+        $('msgError-description').innerHTML = null;
+    }
+
+
+        if (hayErrores) {
+            e.preventDefault();
+        }
+    });
 };
-
-
-//imagen
-/*  window.onload = function(e){
-    $('mainImage').addEventListener('change', function(e){
-        const fileInput = this;
-        const errorMessage = $('msgError-mainImage');
-
-        // Limpiar mensajes de error previos
-        errorMessage.innerHTML = null;
-        fileInput.classList.remove('is-invalid');
-        fileInput.classList.remove('is-valid');
-
-        // Verificar si se seleccionó un archivo
-        if (!fileInput.files || fileInput.files.length === 0) {
-            errorMessage.innerHTML = "Debe seleccionar una imagen";
-            fileInput.classList.add('is-invalid');
-            return;
-        }
-
-        // Verificar si el tipo de archivo es una imagen
-        const allowedTypes = ['image/jpeg', 'image/png', 'image/gif'];
-        const selectedType = fileInput.files[0].type;
-        if (!allowedTypes.includes(selectedType)) {
-            errorMessage.innerHTML = "Formato de imagen no válido. Seleccione una imagen JPEG, PNG o GIF.";
-            fileInput.classList.add('is-invalid');
-            return;
-        }
-
-        // Si la validación es exitosa, puedes realizar otras acciones si es necesario
-        errorMessage.innerHTML = null;
-        fileInput.classList.add('is-valid'); */
