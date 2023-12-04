1c4b31b24dfa8ffef465d1d549a6f281f575cec9
GonzaloArmoa <gonzalo-armoa@hotmail.com>
2023-12-04 20:26:24 -0300

    feat: adjust modal cart

--- public/javascripts/cart.js
+++ public/javascripts/cart.js
@@ -1,3 +1,26 @@
+const addItemToCart = async (amount, product) => {
+    try {
+
+        const response = await fetch('/cart', {
+            method : "POST",
+            body : {
+                amount,
+                product
+            }
+        })
+
+        const {ok, cart, message} = await response.json();
+
+        if(!ok){
+            throw new Error(message)
+        }
+        
+    } catch (error) {
+        console.log(error)
+        alert(error.message)       
+    }
+}
+
 window.onload = function () {
 
     $("btn-cart") && $("btn-cart").addEventListener('click', async function (e) {
@@ -5,12 +28,27 @@ window.onload = function () {
         try {
 
             const response = await fetch ('/cart')
-            const result = await response.json();
+            const {ok, cart, message} = await response.json();
 
-            console.log(result)
+            if(ok) {
+                if(cart.products.length) {
+                    $('cart-body').innerHTML = `<p>aqui voy a dibujar los productos</p>`
+                }else {
+                    $('cart-body').innerHTML = `
+                    <div class="alert alert-warning" role="alert">
+                        No hay productos en el carrito.
+                    </div>
+                  `
+
+                }
+               
+            }else {
+                throw new Error(message)
+            }
             
         } catch (error) {
             console.log(error)
+            alert(error.message)
             
         }
     })
--- src/controllers/apiCartController.js
+++ src/controllers/apiCartController.js
@@ -1,22 +1,26 @@
+const db = require('../database/models')
+
 module.exports = {
     showAll : async (req,res) => {
         try {
 
             if(!req.session.cart) {
                 let error = new Error()
-                error.message = 'Sesion no encontrada'
+                error.message = 'Debes loguearte'
                 error.status = 404;
                 throw error
             }
 
             return res.status(200).json({
                 ok : true,
-                cart : req.session.cart
+                cart : req.session.cart,
+                message : "ok"
             })
             
         } catch (error) {
             return res.status(error.status || 500).json({
                 ok : false,
+                cart : null,
                 message : error.message || 'Upss, hubo un error'
             })
             
@@ -25,6 +29,46 @@ module.exports = {
 
     },
     addItem : async (req,res) => {
+        try {
+
+            if(!req.session.cart) {
+                let error = new Error()
+                error.message = 'Debes loguearte'
+                error.status = 404;
+                throw error
+            }
+            console.log(req.body)
+
+            const {amount, product: id} = req.body;
+
+            const {name, image, price, discount} = await db.Product.findByPk(id)
+
+            req.session.cart.products.push({
+                id,
+                name,
+                image,
+                price,
+                discount,
+                amount
+            });
+
+            req.session.cart.total = req.session.cart.products.map(product => product.price * amount).reduce((a,b) => a+b, 0)
+            
+            return res.status(200).json({
+                ok : true,
+                cart : req.session.cart,
+                message : "ok"
+            })
+
+            
+        } catch (error) {
+            return res.status(error.status || 500).json({
+                ok : false,
+                cart : null,
+                message : error.message || 'Upss, hubo un error'
+            })
+            
+        }
 
     },
     removeItem : async (req,res) => {
